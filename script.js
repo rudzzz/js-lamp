7284bbf968636bbfba2451bd9d939f50aaf5583d
Acir Rudson Souza <97038663+rudzzz@users.noreply.github.com>
2022-01-26 23:22:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function on(){
+    let on = document.getElementById('off');
+    on.src="acesa.png";
+}
+
+function off(){
+    let off = document.getElementById('off');
+    off.src="apagada.png";
+}
